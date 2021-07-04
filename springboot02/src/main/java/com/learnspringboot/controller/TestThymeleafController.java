191463325d29ab2d495dfe4a5063586eea4584e1
coderChenMing <280433706@qq.com>
2021-07-04 13:39:27 +0800

    handle exception:默认error.html,自定义异常挑战自定义页面

--- springboot02/src/main/java/com/learnspringboot/controller/TestThymeleafController.java
+++ springboot02/src/main/java/com/learnspringboot/controller/TestThymeleafController.java
@@ -9,11 +9,32 @@ package com.learnspringboot.controller;/*
  */
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
 @Controller
 public class TestThymeleafController {
+    /**设置/templates/index.html为默认首页*/
     @RequestMapping(value = {"/","/index.html"})
     public String index() {
+        String str=null;
+        str.indexOf("a");
         return "index";
+    }
+    @RequestMapping(value = {"/show"})
+    public String show() {
+        int i = 1 / 0;
+        return "index";
+    }
+    /**
+     * 异常处理
+     */
+    @ExceptionHandler(value = {java.lang.ArithmeticException.class})
+    public ModelAndView handArithmeticException(Exception e) {
+        ModelAndView view = new ModelAndView();
+        view.addObject("error", e.toString());
+        view.setViewName("arithmetic");
+        return view;
     }
 }
